2bb387e12ecffa79a80586f76cc592af0219e99b
Matthew Thomason <matthewwtt@gmail.com>
2019-03-04 22:49:00 -0500

    Add type-contextual navigation buttons to tournament pages

--- src/js/Tournament.react.js
+++ src/js/Tournament.react.js
@@ -12,12 +12,21 @@ import Players from './Players.js';
 const Tournament = props => {
   const id = props.params.id;
   const t = Tournaments.byID(id);
+  const surrounding = Tournaments.page(t);
+  const prev = surrounding[0];
+  const next = surrounding[1];
   if (!t) {
     return <NotFound />;
   }
   return (
     <div className="col-md-offset-3 col-md-6">
       <DocumentTitle title={t.name} />
+      <nav aria-label="...">
+        <ul className="pager">
+          {prev ? <li className="previous"><a href={"/tournament/" + prev.id}><span aria-hidden="true">&larr;</span> {prev.name}</a></li> : ''}
+          {next ? <li className="next"><a href={"/tournament/" + next.id}>{next.name} <span aria-hidden="true">&rarr;</span></a></li> : ''}
+        </ul>
+      </nav>
       <div className="page-header pageHeader">
         <h1>{t.coverage ? <a href={t.coverage}>{t.name}</a> : t.name}</h1>
         <p className="lead tournamentLead">{t.formats.join(' / ')}</p>
