bd65a5d985da206197fa3d84ff7b3948581c2d57
Florian Koch <koch.florian@gmx.de>
2020-08-02 14:19:41 +0200

    make pt finals appear on the landing page

--- src/js/utils.js
+++ src/js/utils.js
@@ -5,7 +5,7 @@ import accounting from 'accounting';
 export const formatMoney = amount =>
   amount && accounting.formatMoney(amount, '$', 0);
 
-export const filterOnlyProTours = item => item.type === 'Pro Tour';
+export const filterOnlyProTours = item => (item.type === 'Pro Tour' || item.type === 'Player Tour Finals Online');
 
 export const filterOnlyProLeagues = item => (item.type === 'Magic Pro League' || item.type === 'Rivals League');
 
