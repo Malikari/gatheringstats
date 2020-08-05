d388e6f057517fc7b11051159a6c8a6f6d9343b1
Florian Koch <koch.florian@gmx.de>
2020-08-05 18:48:13 +0200

    integration of nationals and removing players tour finals from 'other tournament' section

--- src/js/utils.js
+++ src/js/utils.js
@@ -11,4 +11,4 @@ export const filterOnlyProLeagues = item => (item.type === 'Magic Pro League' ||
 
 export const filterOnlyGrandPrix = item => item.type === 'Grand Prix';
 
-export const filterOtherTournaments = item => (item.type !== 'Pro Tour' && item.type !== 'Grand Prix' && item.type !== 'Magic Pro League' && item.type !== 'Rivals League');
+export const filterOtherTournaments = item => (item.type !== 'Pro Tour' && item.type !== 'Grand Prix' && item.type !== 'Magic Pro League' && item.type !== 'Rivals League' && item.type !== 'Player Tour Finals Online');
